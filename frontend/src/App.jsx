03298eaf41271876ac98b99ab4b0cbb59bf18a95
Tejarao <tejaraomutte@gmail.com>
2025-09-20 09:48:49 +0530

    Hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import ProductCard from "./ProductCard"
 import NavBar from "./NavBar"
 import SignUp from "./SignUp"
+import Login from "./Login"
+import Home from "./Home"
 function App(){
   
   return(
     <div className="">
       <NavBar/>
+      <Home/>
       <SignUp/>
+      <Login/> 
+      {/* <ProductCard/> */}
     </div>
   )
 }
